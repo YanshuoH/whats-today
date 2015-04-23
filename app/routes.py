9eb3ccf3ac79e71a0c67fe79f12923b8e037e396
YanshuH <hys235606751@gmail.com>
2015-04-24 00:21:36 +0200

    app init py and routes

---
+++ app/routes.py
@@ -0,0 +1,26 @@
+from app import app
+
+@app.route('/')
+def index():
+    return render_template('login.html',
+                           title='Login')
+
+@app.route('/main')
+def main():
+    return render_template('main.html',
+                           title='Main')
+
+@app.route('/list')
+def list():
+    return render_template('list.html',
+                           title='List')
+
+@app.route('/today')
+def today():
+    return render_template('today.html',
+                           title='Today')
+
+@app.route('/edit')
+def edit():
+    return render_template('edit.html',
+                           title='Edit')
