1a39e051675e8caa4855973d6c63182dd5e03f25
YanshuoH <hys235606751@gmail.com>
2015-04-24 16:52:36 +0200

    setup dev/prod env

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,11 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
+
+# ENV
+env = os.environ.get('WHATSTODAY_ENV', 'dev')
+app.config.from_object('app.settings.%sConfig' % env.capitalize())
+app.config['ENV'] = env
+
 from app import routes
