828395e14edd14bba336d93133ca74ad3dd963de
YanshuoH <hys235606751@gmail.com>
2015-04-24 17:33:35 +0200

    setup assets

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 import os
 from flask import Flask
+from flask_assets import Environment
+from webassets.loaders import PythonLoader as PythonAssetsLoader
 
 app = Flask(__name__)
 
@@ -7,5 +9,13 @@ app = Flask(__name__)
 env = os.environ.get('WHATSTODAY_ENV', 'dev')
 app.config.from_object('app.settings.%sConfig' % env.capitalize())
 app.config['ENV'] = env
+
+# Assets
+import assets
+assets_env = Environment(app)
+assets_loader = PythonAssetsLoader(assets)
+for name, bundle in assets_loader.load_bundles().iteritems():
+    assets_env.register(name, bundle)
+
 
 from app import routes
---
+++ app/assets.py
@@ -0,0 +1,18 @@
+from flask_assets import Bundle
+
+css = Bundle(
+    'built/bootstrap.css',
+    Bundle(
+        'sass/main.scss',
+        filters='pyscss'
+    ),
+    filters='cssmin', output='built/common.css')
+
+js = Bundle(
+    'components/jquery/dist/jquery.min.js',
+    'components/bootstrap/dist/js/bootstrap.min.js',
+    # Bundle(
+    #     'js/main.js',
+    #     filters='closure_js'
+    # ),
+    output='built/common.js')
