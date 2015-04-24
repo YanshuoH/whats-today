3894d4352c7d2c0a73ead57fdd0b44d241425f74
YanshuH <hys235606751@gmail.com>
2015-04-24 21:34:31 +0200

    facebook connect

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,23 @@
 import os
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.login import LoginManager
 from flask_assets import Environment
 from webassets.loaders import PythonLoader as PythonAssetsLoader
 
 app = Flask(__name__)
 
-# ENV
+# ENV configuration
 env = os.environ.get('WHATSTODAY_ENV', 'dev')
 app.config.from_object('app.settings.%sConfig' % env.capitalize())
 app.config['ENV'] = env
+
+# DB
+db = SQLAlchemy(app)
+
+# Login manager
+lm = LoginManager()
+lm.init_app(app)
 
 # Assets
 import assets
@@ -18,4 +27,4 @@ for name, bundle in assets_loader.load_bundles().iteritems():
     assets_env.register(name, bundle)
 
 
-from app import views
+from app import views, models
--- app/views.py
+++ app/views.py
@@ -1,5 +1,10 @@
-from flask import render_template
 from app import app
+from models import User
+from oauth import OAuthSignIn
+from flask import render_template
+from flask.ext.login import login_user, logout_user, current_user, \
+    login_required
+
 
 @app.route('/')
 def index():
@@ -25,3 +30,27 @@ def today():
 def edit():
     return render_template('form.html',
                            title='Edit')
+
+@app.route('/authorize/<provider>')
+def oauth_authorize(provider):
+    if not current_user.is_anonymous():
+        return redirect(url_for('index'))
+    oauth = OAuthSignIn.get_provider(provider)
+    return oauth.authorize()
+
+@app.route('/callback/<provider>')
+def oauth_callback(provider):
+    if not current_user.is_anonymous():
+        return redirect(url_for('index'))
+    oauth = OAuthSignIn.get_provider(provider)
+    social_id, username, email = oauth.callback()
+    if social_id is None:
+        flash('Authentication failed.')
+        return redirect(url_for('index'))
+    user = User.query.filter_by(social_id=social_id).first()
+    if not user:
+        user = User(social_id=social_id, nickname=username, email=email)
+        db.session.add(user)
+        db.session.commit()
+    login_user(user, True)
+    return redirect(url_for('main'))
